999a1e82f805dfc69ad20c46fc12c12907573b40
Anatolii <aazz@i.ua>
2015-12-20 05:47:16 +0200

    второе задание

--- src/lesson3/flowerBouquet/Rosebush.java
+++ src/lesson3/flowerBouquet/Rosebush.java
@@ -3,14 +3,17 @@ package lesson3.flowerBouquet;
 public class Rosebush {
     public Rose[] rose;
     int count;
+
     public Rosebush() {
        rose = new Rose[10];
        count = 0;
     }
+
     public void addRose(Rose rose) {
         this.rose[count+1] = rose;
         count++;
     }
+
     public void info() {
         if (count > 0) {
             System.out.println("Rosebush contain " + count + " roses color: " + rose[count].color + " " + rose[count].spike);
