a6dbc823ed56f10daf4e6ddbb9d4d4310eceeed9
Olga Kulikova <masyakot@gmail.com>
2015-12-21 22:58:21 +0200

    please look at my comments

--- src/lesson4/temperatire/Temperature.java
+++ src/lesson4/temperatire/Temperature.java
@@ -1,11 +1,11 @@
 package lesson4.temperatire;
 
 public class Temperature {
-    public static double fahrenheit(double celcius) {
+    public static double fahrenheit(double celcius) {//Гугл в помощь Celsius
         return celcius * 1.8 + 32;
     }
 
-    public static double celcius(double fahrenheit) {
+    public static double celcius(double fahrenheit) {//методы надо именовать с глагола
         return (fahrenheit-32)/1.8;
     }
 
