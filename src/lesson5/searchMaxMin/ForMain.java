63a7779af0846252d77c22bf7c44e0ee8966c818
Anatolii <aazz@i.ua>
2015-12-22 23:30:17 +0200

    Домашнее задание к 5 модулю, первая часть.

---
+++ src/lesson5/searchMaxMin/ForMain.java
@@ -0,0 +1,11 @@
+package lesson5.searchMaxMin;
+
+public class ForMain {
+    public static void main(String[] args) {
+        int[] intArray = {10, 23, 45, 15, 99, 1, 5};
+
+        System.out.println(SearchMaxMin.getMaxElement(intArray));
+        System.out.println(SearchMaxMin.getMinElement(intArray));
+    }
+
+}
---
+++ src/lesson5/searchMaxMin/SearchMaxMin.java
@@ -0,0 +1,23 @@
+package lesson5.searchMaxMin;
+
+public class SearchMaxMin {
+    public static int getMaxElement(int[] intArray) {
+        int maxElement = intArray[0];
+        for (int i = 1; i < intArray.length-1; i++) {
+            if (maxElement < intArray[i]) {
+                maxElement = intArray[i];
+            }
+        }
+        return maxElement;
+    }
+
+    public static int getMinElement(int[] intArray) {
+        int minElement = intArray[0];
+        for (int i = 1; i < intArray.length-1; i++) {
+            if (minElement > intArray[i]) {
+                minElement = intArray[i];
+            }
+        }
+        return minElement;
+    }
+}
