237adc06a8c9b96aacb6e3471a99ae860807af75
Anatolii <aazz@i.ua>
2015-12-25 01:25:51 +0200

    Домашнее задание к 5 модулю, вторая часть. Сортировка методом Merge O(n)=n*log(n)

--- src/lesson5/mergeSort/MergeSort.java
+++ src/lesson5/mergeSort/MergeSort.java
@@ -2,13 +2,12 @@ package lesson5.mergeSort;
 
 public class MergeSort {
     public static void mergeSort(int[] array, int lo, int hi) {
-        if (hi <= lo) {
-            return;
+        if (hi > lo) {
+            int mid = lo + (hi - lo) / 2;
+            mergeSort(array, lo, mid);
+            mergeSort(array, mid + 1, hi);
+            merge(array, lo, mid, hi);
         }
-        int mid = lo + (hi - lo) / 2;
-        mergeSort(array, lo, mid);
-        mergeSort(array, mid + 1, hi);
-        merge(array, lo, mid, hi);
     }
 
     private static void merge(int[] array, int lo, int mid, int hi) {
