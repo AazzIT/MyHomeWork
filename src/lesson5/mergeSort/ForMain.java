c0612d9898db9a6fc6f1400a230503d317a0e5a3
Olga Kulikova <masyakot@gmail.com>
2015-12-24 23:55:10 +0200

    added comments

--- src/lesson5/mergeSort/ForMain.java
+++ src/lesson5/mergeSort/ForMain.java
@@ -5,7 +5,7 @@ public class ForMain {
         int[] array = new int[20];
 
         System.out.println("Array before sorting: ");
-        for(int i = 0; i < array.length; i++) {
+        for(int i = 0; i < array.length; i++) { //пробел после слова for
             array[i] = (int) Math.round(Math.random() * 100);
             System.out.print(array[i] + " ");
         }
@@ -13,7 +13,7 @@ public class ForMain {
         array = MergeSort.mergeSort(array);
 
         System.out.println("\n\nArray after sorting: ");
-        for(int i = 0; i < array.length; i++) {
+        for(int i = 0; i < array.length; i++) {//можно for each, идея подсвечивает
             System.out.print(array[i] + " ");
         }
         System.out.println("");
