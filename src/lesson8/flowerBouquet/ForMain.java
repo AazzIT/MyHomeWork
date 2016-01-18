8a58f2bf1697190e5111110b6d9d7817e68a9c5a
Anatolii <aazz@i.ua>
2016-01-18 03:40:15 +0200

    Домашнее задание к 8 модулю,  Переписал  fileSystem, flowerBouquet, musicShop в виде коллекций. Печать в  виде таблицы. Добавил сортировку в fileSystem

--- src/lesson8/flowerBouquet/ForMain.java
+++ src/lesson8/flowerBouquet/ForMain.java
@@ -1,12 +1,18 @@
 package lesson8.flowerBouquet;
 
 public class ForMain {
+    public static final String COLOR_RED = "red";
+    public static final String COLOR_WHITE = "white";
+    public static final String COLOR_YELLOW = "yellow";
+    public static final String WITH_SPIKE = "with spike";
+    public static final String WITHOUT_SPIKE = "without spike";
+
     public static void main(String[] args) {
-        Bouquet bouquet = new Bouquet("Bouquet", new Rose("red","with spike"));
-        bouquet.addFlower(new Rose("red","with spike"));
-        bouquet.addFlower(new Rose("red","with spike"));
-        bouquet.addFlower(new Aster("white"));
-        bouquet.addFlower(new Tulip("yellow"));
+        Bouquet bouquet = new Bouquet("Bouquet", new Rose(COLOR_RED,WITH_SPIKE));
+        bouquet.addFlower(new Rose(COLOR_RED,WITH_SPIKE));
+        bouquet.addFlower(new Rose(COLOR_RED,WITH_SPIKE));
+        bouquet.addFlower(new Aster(COLOR_WHITE));
+        bouquet.addFlower(new Tulip(COLOR_YELLOW));
         bouquet.info();
 
         bouquet.removeFlower();
@@ -14,9 +20,9 @@ public class ForMain {
         bouquet.info();
 
         Rosebush rosebush = new Rosebush();
-        rosebush.include(new Rose("white","without spike"));
-        rosebush.include(new Rose("white","without spike"));
-        rosebush.include(new Rose("white","without spike"));
+        rosebush.include(new Rose(COLOR_WHITE,WITHOUT_SPIKE));
+        rosebush.include(new Rose(COLOR_WHITE,WITHOUT_SPIKE));
+        rosebush.include(new Rose(COLOR_WHITE,WITHOUT_SPIKE));
         rosebush.info();
     }
 }
