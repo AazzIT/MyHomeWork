f8b4d74009a8e446f1631fbf5b13bdfd8489f987
Anatolii <aazz@i.ua>
2016-02-03 20:02:04 +0200

    Refactor name package

---
+++ test/lesson10/filework/FileWorkerTest.java
@@ -0,0 +1,24 @@
+package lesson10.filework;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileWorkerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testWriteToFile() throws Exception {
+
+    }
+
+    @Test
+    public void testReadFromFile() throws Exception {
+
+    }
+}
