58e87c3972a05256bb542de29a1d275ac85bc142
Aazz <aazz@i.ua>
2016-02-03 21:40:47 +0200

    Lesson9 TEST

--- test/lesson9/filesystem/CryptographerTest.java
+++ test/lesson9/filesystem/CryptographerTest.java
@@ -1,17 +1,44 @@
 package lesson9.filesystem;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CryptographerTest {
     public static final int CRYPT_POWER = 114;
 
-    @Test
-    public void testEncrypt() throws Exception {
+    private String encryptString;
+    private String decryptString;
+    private Folder rootFolder;
+    private Folder pictureFolder;
+    private Folder audioFolder;
+    private String text;
 
+    @Before
+    public void setUp() throws Exception {
+        rootFolder = new Folder("Root");
+
+        rootFolder.addFile(new TextFile("TextOfSong.txt"));
+
+        pictureFolder = new Folder("PictureFiles");
+        rootFolder.addFile(pictureFolder);
+
+        audioFolder = new Folder("AudioFiles");
+        rootFolder.addFile(audioFolder);
+
+        audioFolder.addFile(new AudioFile("Rock.mp3"));
+        audioFolder.addFile(new AudioFile("Blues.mp3"));
+        audioFolder.addFile(new AudioFile("Jazz.mp3"));
+        audioFolder.addFile(new AudioFile("Classic.mp3"));
+
+        pictureFolder.addFile(new PictureFile("LogoCD.gif"));
     }
 
     @Test
-    public void testDecrypt() throws Exception {
-
+    public void testEncryptDecrypt() throws Exception {
+        this.text = audioFolder.showListOfFiles();
+        this.encryptString=Cryptographer.encrypt(text,CRYPT_POWER);
+        this.decryptString=Cryptographer.decrypt(encryptString,CRYPT_POWER);
+        Assert.assertEquals("OK:", text, decryptString);
     }
 }
