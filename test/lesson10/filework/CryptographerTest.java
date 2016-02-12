76216dffbb8e92296d21518fe0aa88b7375614f5
Olga Kulikova <masyakot@gmail.com>
2016-02-12 09:52:55 +0200

    added comments to tests

--- test/lesson10/filework/CryptographerTest.java
+++ test/lesson10/filework/CryptographerTest.java
@@ -8,7 +8,11 @@ public class CryptographerTest {
     public static final boolean ENCRYPT = true;
     public static final boolean DECRYPT = false;
 
-    String text = "... Ad cogitandum et agendum homo natus est ...";
+    String text = "... Ad cogitandum et agendum homo natus est ..."; //То, что ты их сделал не приватными - в этом должен быть смысл
+    //Разве ты их будешь использовать в других классах, вызывая отсюда? Тебе идея в предыдущей версии предлагала сделать их
+    //локальными, потому что ты их использовал только один раз в одном методе с уже присвоенными значениями, больше похоже
+    // на константы. Это не есть обязательное условие - делать их локальными,
+    //Это было всего лишь предложение. Но делать их не приватными - на это должен быть спрос, а не просто так.))
     String cryptedText = "... Kn myqsdkxnew od kqoxnew rywy xkdec ocd ...";
 
     @Test(timeout = 3000)
