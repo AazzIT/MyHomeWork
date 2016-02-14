f0c05bda5b09d3329ed829925463ad892c3e63e2
Anatolii <aazz@i.ua>
2016-02-14 02:55:49 +0200

    Refactor name package

--- test/lesson10/filework/FileWorkerTest.java
+++ test/lesson10/filework/FileWorkerTest.java
@@ -8,16 +8,13 @@ public class FileWorkerTest {
     public static final boolean ENCRYPT = true;
     public static final String FILE_NAME = "CryptedText.txt";
 
-    String text = "... Ad cogitandum et agendum homo natus est ...";
-    String cryptedText = "... Kn myqsdkxnew od kqoxnew rywy xkdec ocd ...";
-
     @Test
     public void testWriteReadFile() throws Exception {
+        String text = "... Ad cogitandum et agendum homo natus est ...";
         text = Cryptographer.crypt(text, CRYPT_POWER, ENCRYPT);
         FileWorker.writeToFile(FILE_NAME, text);
-
         text = FileWorker.readFromFile(FILE_NAME);
-
-        Assert.assertEquals(cryptedText, text);
+        String cryptedtext = "... Kn myqsdkxnew od kqoxnew rywy xkdec ocd ...";
+        Assert.assertEquals(cryptedtext, text);
     }
 }
