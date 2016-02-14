f15294b7279ce89c7b13fddd88ea85614ccddbb6
Anatolii <aazz@i.ua>
2016-02-14 02:59:52 +0200

    Refactor name package

--- test/lesson10/filework/CryptographerTest.java
+++ test/lesson10/filework/CryptographerTest.java
@@ -7,18 +7,18 @@ public class CryptographerTest {
     public static final int CRYPT_POWER = 114;
     public static final boolean ENCRYPT = true;
     public static final boolean DECRYPT = false;
-    private final String TEXT = "... Ad cogitandum et agendum homo natus est ...";
-    private final String CRYPTEDTEXT = "... Kn myqsdkxnew od kqoxnew rywy xkdec ocd ...";
+    private String text = "... Ad cogitandum et agendum homo natus est ...";
+    private String cryptedtext = "... Kn myqsdkxnew od kqoxnew rywy xkdec ocd ...";
 
     @Test(timeout = 3000)
     public void testEncrypt() throws Exception {
-        String result = Cryptographer.crypt(TEXT, CRYPT_POWER, ENCRYPT);
-        Assert.assertEquals(CRYPTEDTEXT, result);
+        String result = Cryptographer.crypt(text, CRYPT_POWER, ENCRYPT);
+        Assert.assertEquals(cryptedtext, result);
     }
 
     @Test(timeout = 3000)
     public void testDecrypt() throws Exception {
-        String result = Cryptographer.crypt(CRYPTEDTEXT, CRYPT_POWER, DECRYPT);
-        Assert.assertEquals(TEXT, result);
+        String result = Cryptographer.crypt(cryptedtext, CRYPT_POWER, DECRYPT);
+        Assert.assertEquals(text, result);
     }
 }
